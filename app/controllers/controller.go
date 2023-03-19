0c314068966f3f9f2373282c6818e7749505cd4e
Albin George <albinin0002@gmail.com>
2023-03-19 18:51:19 +0100

    Use the repo pattern for db implementation

--- app/controllers/controller.go
+++ app/controllers/controller.go
@@ -5,35 +5,41 @@ import (
 	"net/http"
 	"net/url"
 
+	commentrepo "github.com/albingeorge/commently-service/repo/comments"
+	urlrepo "github.com/albingeorge/commently-service/repo/urls"
 	"github.com/gin-gonic/gin"
 )
 
-var Db map[string][]string
-
-// type Comment string
+var urlRepo urlrepo.Repository
+var commentRepo commentrepo.Repository
 
 type CommentInput struct {
 	Url  string `json:"url" binding:"required"`
 	Text string `json:"text" binding:"required"`
 }
 
+func init() {
+	urlRepo = urlrepo.GetRepo()
+	commentRepo = commentrepo.GetRepo()
+}
+
 func CommentCreate(c *gin.Context) {
-	// init Db
-	if Db == nil {
-		Db = map[string][]string{}
-	}
 	var commentJsonInput CommentInput
 
 	if c.Bind(&commentJsonInput) == nil {
-		if currentComments, ok := Db[commentJsonInput.Url]; ok {
-			Db[commentJsonInput.Url] = append(currentComments, commentJsonInput.Text)
-		} else {
-			Db[commentJsonInput.Url] = []string{commentJsonInput.Text}
+		urlEntity := urlRepo.Create(commentJsonInput.Url)
+		commentRepo.Create(urlEntity.Id, commentJsonInput.Text)
+
+		urls := urlRepo.Get()
+		urlStr := make([]string, len(urls))
+		i := 0
+		for _, val := range urls {
+			urlStr[i] = val.Name
+			i++
 		}
+		log.Printf("Current Db:\n%v", urlStr)
 
-		log.Printf("Current Db:\n%+v", Db)
-
-		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		c.JSON(http.StatusOK, gin.H{"status": "success"})
 		return
 	}
 
@@ -41,25 +47,44 @@ func CommentCreate(c *gin.Context) {
 }
 
 func CommentFetch(c *gin.Context) {
+	// Parse input
 	inputUrl := c.Query("url")
 	log.Printf("Input URL: %q", inputUrl)
 	decodedValue, err := url.QueryUnescape(inputUrl)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"invalid urlencoded value": inputUrl})
-		log.Fatal(err)
+		log.Print(err)
+		return
 	}
 
 	log.Printf("Decoded URL: %q", decodedValue)
 
-	comments := Db[decodedValue]
-	c.JSON(http.StatusOK, gin.H{"comments": comments})
+	// Fetch url from db
+	urlEntity, err := urlRepo.Fetch(decodedValue)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"status": "failure"})
+		log.Print(err)
+		return
+	}
+
+	// Fetch comments from db
+	comments := commentRepo.GetAll(urlEntity.Id)
+	commentsStr := make([]string, len(comments))
+	i := 0
+	for _, val := range comments {
+		commentsStr[i] = val.Text
+		i++
+	}
+
+	c.JSON(http.StatusOK, gin.H{"comments": commentsStr})
 }
 
 func UrlsFetch(c *gin.Context) {
-	urls := make([]string, len(Db))
+	urls := urlRepo.Get()
+	urlStr := make([]string, len(urls))
 	i := 0
-	for k := range Db {
-		urls[i] = k
+	for _, val := range urls {
+		urlStr[i] = val.Name
 		i++
 	}
 	c.JSON(http.StatusOK, gin.H{"urls": urls})
