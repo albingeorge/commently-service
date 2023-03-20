26a373f977941bf52009ebe23e94ca55bc749609
Albin George <albinin0002@gmail.com>
2023-03-20 22:48:27 +0100

    Cleanup routes

--- app/router/router.go
+++ app/router/router.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/albingeorge/commently-service/controllers"
 	"github.com/gin-gonic/gin"
 )
@@ -10,25 +8,7 @@ import (
 var db = make(map[string]string)
 
 func SetupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
 	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
 
 	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
 		"albin": "temp123", // user:foo password:bar
